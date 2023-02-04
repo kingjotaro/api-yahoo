911b0938cb290509f525e944b4d7f7ffe45c6e1c
Rafael Lourenço <rafaelleet@gmail.com>
2023-02-04 03:18:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+// config inicial
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+
+// leitura do json
+app.use( 
+    express.urlencoded({
+        extended: true,
+    }),
+)
+app.use(express.json())
+
+// rotas da api
+const dataBaseRoute = require('./routes/dataBaseRoute')
+
+app.use('/database', dataBaseRoute)
+
+// rota inical = endpoint
+app.get('/', (req, res) => {
+    //req
+    res.json({msg: 'Funfou!!!'})
+})
+// porta e conexão com db
+const user = process.env.user  
+const password = process.env.password
+
+
+mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.qrw4pbb.mongodb.net/database?retryWrites=true&w=majority`)
+    
+    .then(() => {
+        console.log('conected mongodb')
+        app.listen(3000)
+    })
+    .catch((err) => console.log(err))
+
+
+
