e4d4c5600d27c65d0282ef0e4d375f9d38e368e3
Rafael Lourenço <rafaelleet@gmail.com>
2023-02-04 04:22:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 require('dotenv').config()
+mongoose.set('strictQuery', true);
 
 
 // leitura do json
